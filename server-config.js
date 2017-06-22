1f668d6f5da15585d69779021c4ef293bd703571
David Berry <david@davidberry.io>
2017-06-22 16:27:47 -0700

    backend skeleton complete (#12)
    * random commit
    
    * blahy
    
    * removed idea folder
    
    * backend skeleton complete

---
+++ server-config.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var partials = require('express-partials');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+
+var handler = require('./lib/request-handler');
+
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(partials());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', handler.getIndex);
+app.get('/profile', handler.getProfile);
+
+app.get('/login', handler.getLogin);
+app.post('/login', handler.postLogin);
+app.get('/logout', handler.getLogout);
+
+app.get('/signup', handler.getSignup);
+app.post('/signup', handler.postSignup);
+app.get('/*', handler.get404);
+
+module.exports = app;
