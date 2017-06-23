c6e80b31ee58d4e87234fcbd104d7d1c7c751ab0
johnmrox <johnmrox@gmail.com>
2017-06-22 20:25:27 -0400

    models up and running

---
+++ models/users.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+	username: String,
+	password: String,
+	gender: String,
+	age: Number,
+	zipCode: Number,
+	interests: [],
+	requestHistory: []
+});
+
+var User = mongoose.model('User', userSchema);
+
+// var newUser = new User ({
+// 	username: "john doe1",
+// 	password: "123",
+// 	gender: "male",
+// 	age: 30,
+// 	zipCode: 33456,
+// 	interests: [],
+// 	requestHistory: []	
+// }).save();
+
+
+module.exports = User;
