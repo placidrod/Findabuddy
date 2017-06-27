6d1731eab90e9ef4a585a2233b0f781f205b07c0
Mark <suyatdesign@gmail.com>
2017-06-27 07:19:34 -0700

    Added other search filter

--- public/components/dynamic-content.jsx
+++ public/components/dynamic-content.jsx
@@ -7,15 +7,14 @@ class DynamicContent extends React.Component {
 
     }
 
-    handleSubmitRequest(data) {
-      console.log('You submitted a form', data)
-    }
+
 
     render() {
       return (
         <div className="searchRequest">
           <h1>Search Form</h1>
           <SearchForm onSubmit={this.handleSubmitRequest}/>
+          <SearchList onSubmit={this.handleSubmitRequest}/>
         </div>
       );
 
