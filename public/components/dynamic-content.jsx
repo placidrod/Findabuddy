d6bbf1680f7145124e7801bb8c6d2dff8d5dd293
Mark <suyatdesign@gmail.com>
2017-06-27 10:02:14 -0700

    fix merge conflict

--- public/components/dynamic-content.jsx
+++ public/components/dynamic-content.jsx
@@ -8,9 +8,7 @@ class DynamicContent extends React.Component {
       }
     }
 
-    handleSubmitRequest(data) {
-      console.log('You submitted a form', data)
-    }
+
 
     handlePostClick() {
     }
@@ -18,10 +16,12 @@ class DynamicContent extends React.Component {
     render() {
       return (
         <div className="searchRequest">
+
           {/*<h1>Search Form</h1>*/}
           {/*<SearchForm onSubmit={this.handleSubmitRequest.bind(this)}/>*/}
           <h1>Buddy Request Form</h1>
           <CreateRequest handlePostClick={this.handlePostClick}/>
+
         </div>
       );
 
