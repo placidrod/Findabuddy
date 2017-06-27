c7cf04e819e20886fe297e8eb9c184506f1b29c8
Mark <suyatdesign@gmail.com>
2017-06-27 09:40:22 -0700

    fixed dynamic content conflict

--- public/components/dynamic-content.jsx
+++ public/components/dynamic-content.jsx
@@ -2,10 +2,9 @@ class DynamicContent extends React.Component {
 
     constructor(props) {
       super(props);
-      this.state = {
+      this.state = {};
+      this.handleSubmitRequest = this.handleSubmitRequest.bind(this)
 
-
-      }
     }
 
     handleSubmitRequest(data) {
@@ -18,10 +17,12 @@ class DynamicContent extends React.Component {
     render() {
       return (
         <div className="searchRequest">
+
           {/*<h1>Search Form</h1>*/}
           {/*<SearchForm onSubmit={this.handleSubmitRequest.bind(this)}/>*/}
           <h1>Buddy Request Form</h1>
           <CreateRequest handlePostClick={this.handlePostClick}/>
+
         </div>
       );
 
--- public/components/search-options.jsx
+++ public/components/search-options.jsx
@@ -3,61 +3,160 @@ class SearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ActivityVerb: 'Verb',
-      ActivityNoun: 'Noun'
+      ActivityVerb: '',
+      ActivityNoun: '',
+      ActivityDate: '',
+      ActivityTime: '',
+      ZipCode: '',
+      Age: {
+        $gte: '',
+        $lte: ''
+      },
+      Gender: '',
+      Description: ''
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    // this.handleAgeInput = this.handleInput.bind(this);
   }
 
+  handleInput(e){
+    var target = e.target;
+    var name = target.name;
+    console.log('name', name, 'value', target.value);
 
-  handleActivityVerb(e){
-    console.log(e.target.value);
-    this.setState({
-      ActivityVerb: e.target.value,
-    })
+    if (name === '$gte') {
+      this.setState({
+        Age: {'$gte': target.value},
+      });
+      console.log(this.state)
+    } else if (name === '$lte'){
+      this.setState({
+        Age: {'$lte': target.value},
+      });
+      console.log(this.state)
+    } else {
+      this.setState({
+        [name]: target.value,
+      });
+    }
+
   }
 
-  handleActivityNoun(e){
-    console.log(e.target.value);
-    this.setState({
-      ActivityNoun: e.target.value,
-    })
-  }
-
-  //  handleInput(e){
-  //   console.log(e.target.value.AcvtivityVerb);
-  //   console.log(e.target.value.AcvtivityVerb);
-  //   this.setState({
-  //     ActivityVerb: e.target.value.ActivityVerb,
-  //     ActivityVerb: e.target.value.AcvivityNoun,
-  //   })
+  // handleAgeInput(e){
+  //   var target = e.target;
+  //   var name = target.name;
+  //   console.log(this.state)
+  //   if (name === '$gte') {
+  //     this.setState({
+  //       Age: {'$gte': target.value},
+  //     });
+  //   } else if (name === '$lte'){
+  //     this.setState({
+  //       Age: {'$lte': target.value},
+  //     });
+  //   }
   // }
 
   handleSubmit(e) {
+    // this.props.handleSubmitRequest(this);
     e.preventDefault();
-    console.log(this)
-  }
+    var self = this;
+    var age = this.state.Age;
 
+    console.log(age)
+    // fetch('http://127.0.0.1:3000/buddyRequest', {
+    //     method: 'POST',
+    //     data: {
+    //       ActivityVerb: self.ActivityVerb,
+    //       ActivityNoun: self.ActivityNoun,
+    //       ActivityDate: self.ActivityDate,
+    //       ActivityTime: self.ActivityTime,
+    //       ZipCode: self.ZipCode,
+    //       Age: {
+    //         $gte: age.$gte,
+    //         $lte: age.$lte
+    //       },
+    //       Description: self.Description
+    //     }
+    //   })
+    //   .then(function(response) {
+    //     return response.json()
+    //   }).then(function(body) {
+    //     console.log(body);
+    // });
+  }
 
 
   render() {
     return (
-
-      <form className="form-horizon" onSubmit={this.handleSubmit.bind(this)}>
+      <form className="form-horizontal" onSubmit={this.handleSubmit}>
         <div className="form-group">
-          <label htmlFor="verb">Activty Verb</label>
+          <label className="control-label" htmlFor="verb">Activty Verb</label>
           <input type="text" className="form-control"
-            onChange={this.handleActivityVerb.bind(this)}
+            name="ActivityVerb"
+            onChange={this.handleInput}
             value={this.state.ActivityVerb}/>
         </div>
         <div className="form-group">
-          <label htmlFor="noun">Activity Noun</label>
+          <label className="control-label" htmlFor="noun">Activity Noun</label>
           <input type="text" className="form-control"
-            onChange={this.handleActivityNoun.bind(this)}
+            name="ActivityNoun"
+            onChange={this.handleInput}
             value={this.state.ActivityNoun}/>
         </div>
 
+
         <div className="form-group">
-          <div className="col-sm-offset-2 col-sm-10">
+          <label className="control-label" htmlFor="date">Date</label>
+          <input type="text" className="form-control"
+            name="ActivityDate"
+            onChange={this.handleInput}
+            value={this.state.ActivityDate}/>
+        </div>
+        <div className="form-group">
+          <label className="control-label" htmlFor="time">Time</label>
+          <input type="password" className="form-control"
+            name="ActivityTime"
+            onChange={this.handleInput}
+            value={this.state.ActivityTime}/>
+        </div>
+        <div className="form-group">
+          <label className="control-label" htmlFor="zip">Zip</label>
+          <input type="text" className="form-control"
+            name="ZipCode"
+            onChange={this.handleInput}
+            value={this.state.ZipCode} />
+        </div>
+        <div className="form-group">
+          <label className="control-label age" htmlFor="age">Age Min</label>
+          <input type="text" className="form-control input-medium"
+            name="$gte"
+            onChange={this.handleInput}
+            value={this.state.Age.$gte} />
+          <label className="control-label age" htmlFor="age">Age Max</label>
+          <input type="text" className="form-control input-medium"
+            name="$lte"
+            onChange={this.handleInput}
+            value={this.state.Age.$lte} />
+          <label className="control-label" htmlFor="age">Select Gender</label>
+          <select className="form-control">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="No Preference">No Preference</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label className="control-label" htmlFor="info">Description</label>
+          <input type="text" className="form-control"
+            name="Description"
+            onChange={this.handleInput}
+            value={this.state.Description} />
+        </div>
+
+        <div className="form-group">
+          <div className="col-sm-offset-10">
             <button type="submit" className="btn btn-default">Submit</button>
           </div>
         </div>
@@ -67,26 +166,7 @@ class SearchForm extends React.Component {
 
 }
 
-
-
-  /*var searchObj = {
-  ActivityVerb: this.searchActivityVerb,
-  ActivityNoun: this.searchAcvitityNoun,
-  ActivityDate: this.searchActivityDate,
-  ActivityTime: this.searchActivityTime,
-  Zip: this.zipCode,
-  Age: {}
-  Description: this.searchDescription,
-
-  {age: {‘$gte’: 21, ‘$lte’: 30}}
-
-  }*/
-
-
 //POST Request Activity Search
-
-
-
 
 
 window.SearchForm = SearchForm;
