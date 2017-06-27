10afd6acaaacdf77d2180b6e579d7f91d5bbf9ae
Mark <suyatdesign@gmail.com>
2017-06-27 07:19:34 -0700

    Added other search filter

---
+++ public/components/search-result-list.jsx
@@ -0,0 +1,46 @@
+class SearchList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  searchHandler() {
+
+  }
+
+  requestFormHander() {
+
+  }
+
+  profileHandler() {
+
+  }
+
+  logoutHandler() {
+
+  }
+
+  render() {
+    return (
+      <div className="mainApp">
+        <Nav />
+        <div className="dynamicContent">
+
+          <DynamicContent
+
+
+          />
+
+        </div>
+        <div className="notificationWindow">
+
+        </div>
+      </div>
+    );
+  }
+
+}
+
+window.SearchList = SearchList;
