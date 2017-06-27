533502fb64d7b189582337b80abe9140fb632852
johnmrox <johnmrox@gmail.com>
2017-06-27 17:54:56 -0400

    buddy request fields clear on submit. new request and search buttons functional

--- public/components/dynamic-content.jsx
+++ public/components/dynamic-content.jsx
@@ -1,33 +1,35 @@
 class DynamicContent extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {};
-      this.handleSubmitRequest = this.handleSubmitRequest.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleSubmitRequest = this.handleSubmitRequest.bind(this);
+  }
 
-    }
+  handleSubmitRequest(data) {
+    console.log('You submitted a form', data)
+  }
 
-    handleSubmitRequest(data) {
-      console.log('You submitted a form', data)
-    }
+  handlePostClick() {
+  }
 
-    handlePostClick() {
-    }
-
-    render() {
+  render() {
+    if (this.props.renderSearch) {
       return (
         <div className="searchRequest">
-
-          {/*<h1>Search Form</h1>*/}
-          {/*<SearchForm onSubmit={this.handleSubmitRequest.bind(this)}/>*/}
-          <h1>Buddy Request Form</h1>
-          <CreateRequest handlePostClick={this.handlePostClick}/>
-
+          <h1>Search Form</h1>
+          <SearchForm onSubmit={this.handleSubmitRequest.bind(this)}/>
         </div>
       );
-
+    } else {
+      return (
+        <div className="searchRequest">
+          <h1>Buddy Request Form</h1>
+          <CreateRequest handlePostClick={this.handlePostClick.bind(this)}/>
+        </div>
+      );
     }
-
+  }
 }
 
 window.DynamicContent = DynamicContent;
