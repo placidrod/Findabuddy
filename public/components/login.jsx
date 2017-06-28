4e5f92a39d1e6a70188d7a6dd258c9bf1c758578
david <david@davidberry.io>
2017-06-27 22:50:45 -0700

    signup and login frontend functional

--- public/components/login.jsx
+++ public/components/login.jsx
@@ -35,25 +35,28 @@ class Login extends React.Component {
         }
       });
   }
+  goSignup() {
+    window.location.replace('http://localhost:3000/signup');
+
+  }
   render() {
     return (
       <div className="panel panel-default col-md-2">
         Login
         <input
           onChange={(e) => this.usernameOnChangeHandler(e.target.value)}
-          value="john doe1"
           className="form-control col-md-2"
           type="text"
         />
         <input
           onChange={(e) => this.passwordOnChangeHandler(e.target.value)}
           type="password"
-          value="123"
           className="form-control col-md-2"
-          type="text"
         />
         <button className="button col-md-6"
-          onClick={() => this.submitRequest()}>Submit</button>
+          onClick={() => this.submitRequest()}>Login</button>
+        <button className="button col-md-6"
+                onClick={() => this.goSignup()}>Signup</button>
       </div>
     );
   }
---
+++ public/components/signup.jsx
@@ -0,0 +1,66 @@
+class Signup extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      'username': 'odb',
+      'password': '123'
+    };
+  }
+
+  usernameOnChangeHandler(username) {
+    //this.setState({'username': event.trigger.
+    //console.log('change handler username', username);
+    this.setState({ username });
+  }
+  passwordOnChangeHandler(newVal) {
+    //this.setState({'username': event.trigger.
+    //console.log('change handler password', newVal);
+    this.setState({'password': newVal});
+  }
+  submitSignup() {
+    $.ajax({
+      url: 'http://localhost:3000/signup',
+      type: 'POST',
+      data: this.state,
+      dataType: 'application/json',
+    })
+      .done(function(data) {
+        console.log('signup success ',data);
+        if (data.status === 200) {
+          window.location.replace('http://localhost:3000');
+        }
+      })
+      .fail(function(failInfo) {
+        console.log('FAIL',failInfo);
+        if (failInfo.status === 200) {
+          window.location.replace('http://localhost:3000');
+        }
+      });
+  }
+  render() {
+    return (
+      <div className="panel panel-default col-md-2">
+        Signup
+        <input
+          onChange={(e) => this.usernameOnChangeHandler(e.target.value)}
+          className="form-control col-md-2"
+          type="text"
+        />
+        <input
+          onChange={(e) => this.passwordOnChangeHandler(e.target.value)}
+          type="password"
+          className="form-control col-md-2"
+        />
+        <button className="button col-md-6"
+                onClick={() => this.submitSignup()}>Signup</button>
+      </div>
+    );
+  }
+
+}
+
+
+ReactDOM.render(
+  <Signup />, document.getElementById('signup')
+);
