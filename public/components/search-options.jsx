b1cc21b150d3ecd912247bc0adf928ab924efbc5
david <david@davidberry.io>
2017-06-28 09:40:36 -0700

    search-options mods

--- public/components/search-options.jsx
+++ public/components/search-options.jsx
@@ -8,36 +8,36 @@ class SearchForm extends React.Component {
       activityDate: '',
       activityTime: '',
       zipCode: '',
-      age: {
-        $gte: '',
+    age: {
+      $gte: '',
         $lte: ''
-      },
-      gender: 'No Preference',
+    },
+    gender: 'No Preference',
       description: ''
-    };
+  };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.handleAgeInput = this.handleAgeInput.bind(this);
-    this.handleSubmitRequest = this.props.handleSubmitRequest;
-  }
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleInput = this.handleInput.bind(this);
+  this.handleAgeInput = this.handleAgeInput.bind(this);
+  this.handleSubmitRequest = this.props.handleSubmitRequest;
+}
 
-  handleInput(e){
-    var target = e.target;
-    var name = target.name;
+handleInput(e){
+  var target = e.target;
+  var name = target.name;
 
-      this.setState({
-        [name]: target.value,
-      });
+  this.setState({
+    [name]: target.value,
+  });
 
-  }
+}
 
-  handleAgeInput(e){
-    var target = e.target;
-    var name = target.name;
-    var min = this.state.age.$gte;
-    var max = this.state.age.$lte;
-    // var age = this.state.Age;
+handleAgeInput(e){
+  var target = e.target;
+  var name = target.name;
+  var min = this.state.age.$gte;
+  var max = this.state.age.$lte;
+  // var age = this.state.Age;
 
     if (name === '$gte') {
       this.setState({
@@ -62,6 +62,7 @@ class SearchForm extends React.Component {
 
     e.preventDefault();
     var self = this;
+    console.log('self state: ',self.state);
     // self.handleSubmitRequest(self.state);
     $.ajax({
       url: 'http://localhost:3000/buddyRequest',
