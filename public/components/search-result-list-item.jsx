6bb997752d3e92b18cefb8b72af975967c1079c9
Mark <suyatdesign@gmail.com>
2017-06-28 21:06:10 -0700

    edited search result

--- public/components/search-result-list-item.jsx
+++ public/components/search-result-list-item.jsx
@@ -2,11 +2,13 @@
 var SearchResultItem = ({result}) => (
 
   <tr>
-    <td>{result.title}</td>
+    <td>{result.postTitle}</td>
     <td>{result.description}</td>
     <td>{result.posteDateTime}</td>
     <td>{result.posteDateTime}</td>
     <td>{result.age}</td>
+    <td>{result.gender}</td>
+    <td>{result.zipCode}</td>
   </tr>
 );
 
