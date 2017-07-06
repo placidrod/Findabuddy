77b39fc00f571017a1ab1b4c9066f3b61a9b836c
ScottRudiger <scottrudiger@gmail.com>
2017-07-06 15:11:06 -0700

    (feat) Add tabs to profile page

--- public/components/profile.jsx
+++ public/components/profile.jsx
@@ -8,6 +8,11 @@ class Profile extends React.Component {
       bio: '',
       bioTitle: '',
       bioExist: false,
+      profilePic: '',
+      // interests: interests,
+      // friends: friends,
+      // interactions: interactions,
+      editing: false
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.postProfileInfo = this.postProfileInfo.bind(this);
@@ -70,54 +75,83 @@ class Profile extends React.Component {
     });
   }
 
-  render() {
-
-    if (!this.state.bioExist) {
-      return (
-        <div className="profile">
-          <h3>Say something about yourself</h3>
-          <form className="form">
-            <div className="form-group">
-              <label htmlFor="noun">Profile title</label>
-              <input type="text"
-                value={this.state.bioTitle}
-                className="form-control"
-                placeholder="Enter a quick title about your bio"
-                name="bioTitle"
-                onChange={this.handleInputChange} required/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="noun">User bio</label>
-              <textarea type="text"
-                value={this.state.bio}
-                className="form-control"
-                rows="5"
-                placeholder="Write a brief description about yourself"
-                name="bio"
-                onChange={this.handleInputChange} required>
-              </textarea>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-10">
-                <button type="submit" className="btn btn-default" onClick={this.postProfileInfo}>Submit</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      );
-    } else {
-      return (
-        <div className="profile">
-          <div className="panel panel-warning">
-            <div className="panel-heading"><h4>{this.state.bioTitle}</h4></div>
-            <div className="panel-body">
-              <p>{this.state.bio}</p>
+  showProfileEdit() {
+    return (
+      <div className="profile">
+        <h3>Say something about yourself</h3>
+        <form className="form">
+          <div className="form-group">
+            <label htmlFor="noun">Profile title</label>
+            <input type="text"
+              value={this.state.bioTitle}
+              className="form-control"
+              placeholder="Enter a quick title about your bio"
+              name="bioTitle"
+              onChange={this.handleInputChange} required/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="noun">User bio</label>
+            <textarea type="text"
+              value={this.state.bio}
+              className="form-control"
+              rows="5"
+              placeholder="Write a brief description about yourself"
+              name="bio"
+              onChange={this.handleInputChange} required>
+            </textarea>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+              <button type="submit" className="btn btn-default" onClick={this.postProfileInfo}>Submit</button>
             </div>
           </div>
+        </form>
+      </div>
+    );
+  }
+
+  showProfileView() {
+    return (
+      <div className="profile">
+        <div className="panel panel-warning">
+          <div className="panel-heading"><h4>{this.state.bioTitle}</h4></div>
+          <div className="panel-body">
+            <p>{this.state.bio}</p>
+          </div>
         </div>
-      );
+      </div>
+    );
+  }
+
+  render() {
+    let profile;
+    if (!this.state.bioExist || this.state.editing) {
+      profile = this.showProfileEdit();
+    } else {
+      profile = this.showProfileView();
     }
+
+    return (
+      <div>
+        <ul className="nav nav-tabs" role="tablist">
+          <li role="presentation" className="active"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+          <li role="presentation"><a href="#interests" aria-controls="interests" role="tab" data-toggle="tab">Interests</a></li>
+        </ul>
+        <div className="tab-content">
+          <div role="tabpanel" className="tab-pane active" id="profile">
+            {profile}
+          </div>
+          <div role="tabpanel" className="tab-pane" id="interests">...</div>
+        </div>
+      </div>
+    );
   }
 }
+
+const editInterests = ({}) => {
+
+};
+
+
 
 window.Profile = Profile;
