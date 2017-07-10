7b72989994c1f706766aa08e52455fb0c9a8a13c
Kenneth Marshall <kdmarshall89@gmail.com>
2017-07-10 10:27:18 -0500

    Rearrange tab positions to prevent breakage in map rendering

--- public/components/home.jsx
+++ public/components/home.jsx
@@ -1,23 +1,41 @@
-var Home = ({requests, handlePostClick, handleMarkerClick, handleInfoClose}) => (
-  <div>
-    <ul className="nav nav-tabs" role="tablist">
-      <li role="presentation" className="active"><a href="#browse" aria-controls="browse" role="tab" data-toggle="tab">Browse Requests</a></li>
-      <li role="presentation">
-        <a href="#map" aria-controls="map" role="tab" data-toggle="tab">View Map</a>
-      </li>
-    </ul>
-    <div className="tab-content">
-      <div role="tabpanel" className="tab-pane active" id="browse">
-        <BrowseRequests
-          requests={requests}
-          handlePostClick={handlePostClick}
-        />
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.mapTab;
+
+  }
+
+
+  render(){
+    return (
+      <div>
+        <ul className="nav nav-tabs" role="tablist" id="requestTab">
+
+          <li role="presentation">
+            <a href="#map" id="mapSelector" className="active" aria-controls="map" role="tab" data-toggle="tab">View Map</a>
+          </li>
+
+          <li role="presentation" ><a href="#browse" aria-controls="browse" role="tab" data-toggle="tab">Browse Requests</a>
+          </li>
+
+        </ul>
+        <div className="tab-content">
+
+          <div role="tabpanel" className="tab-pane active" id="map">
+            <MapView requests={this.props.requests} handleMarkerClick={this.props.handleMarkerClick} handleInfoClose={this.props.handleInfoClose} handlePostClick={this.props.handlePostClick} />
+          </div>
+
+          <div role="tabpanel" className="tab-pane" id="browse">
+            <BrowseRequests
+              requests={this.props.requests}
+              handlePostClick={this.props.handlePostClick}
+            />
+          </div>
+
+        </div>
       </div>
-      <div role="tabpanel" className="tab-pane" id="map">
-        <MapView requests={requests} handleMarkerClick={handleMarkerClick} handleInfoClose={handleInfoClose} handlePostClick={handlePostClick} />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 window.Home = Home;
